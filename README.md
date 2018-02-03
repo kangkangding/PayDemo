bf253acb64112e9762564c712c130ec034882046
dingkk <851615943@qq.com>
2018-02-03 11:47:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# PayDemo 
+* 支付宝支付<br/>
+用户已安装支付宝支付流程
+步骤1：用户在商家App中选择商品下单、确认购买，进入支付环节，选择支付宝，用户点击确认支付，如图1；
+步骤2：进入到支付宝页面，调起支付宝支付，出现确认支付界面，如图2；
+步骤3：用户确认收款方和金额，点击立即支付后出现输入密码界面，如图3；
+步骤4：输入正确密码后，支付宝端显示支付结果，如图4；
+步骤5：自动回跳到商家App中，商家根据付款结果个性化展示订单处理结果，如图5。
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/AndroidEmulator1_0001.gif)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/AndroidEmulator2_0001.gif)
+
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/1.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/3.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/4.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/5.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/6.png)
+用户未安装支付宝支付流程
+步骤1：用户在商家App中选择商品下单、确认购买，进入支付环节，选择支付宝，用户点击确认支付，如图6；
+步骤2：用户未安装支付宝客户端，则调起支付宝网页支付收银台，用户登录支付宝账户，如图7；
+步骤3：登录成功后，进入确认付款页面，如图8；
+步骤4：用户点击确认付款，进入支付密码页面，如下图9；
+步骤5：用户输入密码，完成支付，展示支付结果，如图10。
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/7.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/8.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/9.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/10.png)
+![image](https://github.com/kangkangding/PayDemo/blob/master/screen/11.png)
+
+
+
+
+
---
+++ app/src/main/java/com/example/mama/paydemo/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.mama.paydemo;
+
+import android.database.CursorJoiner;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alipay.sdk.app.PayTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SDK_PAY_FLAG = 0xf1;
+    private TextView tv_text;
+    private Button btn_pay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_pay = (Button) findViewById(R.id.btn_pay);
+        tv_text = (TextView) findViewById(R.id.tv_text);
+
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPayData();
+            }
+        });
+    }
+    //获取服务端参数
+    public void getPayData(){
+        String url = "yoururl?id=1&total=0.01";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(getApplicationContext(),"获取数据失败",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("response","response"+response.toString());
+                //此句如果打印日志，response.body().string()的值只能获取一次
+//                Log.d("response","response"+response.body().string());
+                jieXi(response.body().string());
+            }
+        });
+    }
+
+    //解析参数
+    private void jieXi(String response) {
+        try {
+            JSONObject object = new JSONObject(response);
+            JSONObject object2 = new JSONObject(object.getString("data"));
+            payzhifu(object2.getString("info"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //开始执行支付
+    private void payzhifu(String info) {
+        final String orderInfo = info;   // 订单信息
+        Runnable payRunnable = new Runnable() {
+            @Override
+            public void run() {
+                PayTask alipay = new PayTask(MainActivity.this);
+                //返回参数是map类型，官方实例是string 有错误
+                Map<String,String> result = alipay.payV2(orderInfo,true);
+                Message msg = new Message();
+                msg.what = SDK_PAY_FLAG;
+                msg.obj = result;
+                mHandler.sendMessage(msg);
+            }
+        };
+        // 必须异步调用
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+    //执行结果回掉
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case SDK_PAY_FLAG:
+                    Map<String ,String> map = (Map<String,String>) msg.obj;
+                    tv_text.setText(map.toString());
+                    Toast.makeText(MainActivity.this, "::::::",Toast.LENGTH_LONG).show();
+                    break;
+            }
+
+        }
+    };
+}
